1598b0e835146c2dd5cfe9c2abedd95d9b73e0cc
Fernando J. Martínez <fernmarr@gmail.com>
2022-11-04 17:17:02 -0500

    Pantallas HU02

--- app-assets/js/scripts/pages/auth-login.js
+++ app-assets/js/scripts/pages/auth-login.js
@@ -11,6 +11,21 @@ $(function () {
   'use strict';
 
   var pageLoginForm = $('.auth-login-form');
+  console.log(this.getElementsByClassName('requires-validation'));
+  const form = this.getElementsByClassName('requires-validation')
+  console.log(form);
+  if (form.length > 0) {
+    for (let i=0; i<form.length; i++) {
+      form[i].addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    }
+  }
+
 
   // jQuery Validation
   // --------------------------------------------------------------------
